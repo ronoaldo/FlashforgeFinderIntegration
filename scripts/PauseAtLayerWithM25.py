2778ceac5cfdfc39682fefe5808dac4b43b24823
Ronoaldo Pereira <ronoaldo@gmail.com>
2020-05-27 21:36:13 -0300

    Added new Pause at Height script compatible with Finder.

---
+++ scripts/PauseAtLayerWithM25.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2020 Ronoaldo JLP 
+# The PostProcessingPlugin is released under the terms of the AGPLv3 or higher.
+
+from ..Script import Script
+
+##  Pause at layer with M25.
+class PauseAtLayerWithM25(Script):
+    def getSettingDataString(self):
+        return """{
+            "name": "Pause at layer with M25",
+            "key": "PauseAtLayerWithM25",
+            "metadata": {},
+            "version": 2,
+            "settings":
+            {
+                "layer":
+                {
+                    "label": "Layer",
+                    "description": "Layer number where a pause is added before it starts printing.",
+                    "type": "str",
+                    "default_value": ""
+                }
+            }
+        }"""
+    def execute(self, data):
+        layer = self.getSettingValueByKey("layer")
+        try:
+            layer_num = int(layer) + 1
+            gcode = "; PAUSE AT " + str(layer_num-1)
+            gcode += "\nM25\n"
+            data[layer_num] = data[layer_num] + gcode
+        except:
+            print("[ronoaldo] Invalid layer: '%s'", layer)
+        return data
