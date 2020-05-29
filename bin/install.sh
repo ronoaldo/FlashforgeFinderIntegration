5ba820ee2c5e3a9311faaee2392f3baca17bfae0
Ronoaldo Pereira <ronoaldo@gmail.com>
2020-05-29 17:34:56 -0300

    Added dev install for the printer definitions too.

--- bin/install.sh
+++ bin/install.sh
@@ -28,4 +28,24 @@ for version in $VERSIONS ; do
 			echo "$script already installed."
 		fi
 	done
+
+	if ! [ -e "${version}/definitions/finder.def.json" ] ; then
+		echo "Installing printer definition ..."
+		ln -s "${srcdir}/printer/definitions/finder.def.json" "${version}/definitions/finder.def.json"
+	else
+		echo "finder.def.json already installed."
+	fi
+	if ! [ -e "${version}/extruders/FF_finder_extruder_0.def.json" ] ; then
+		echo "Installing extruder definition ..."
+		ln -s "${srcdir}/printer/extruders/FF_finder_extruder_0.def.json" "${version}/extruders/FF_finder_extruder_0.def.json"
+	else
+		echo "FF_finder_extruder_0.def.json already installed."
+	fi
+	if ! [ -e "${version}/meshes/FlashforgeFinderBed.stl" ] ; then
+		echo "Installing printer definition ..."
+		mkdir -p "${version}/meshes/"
+		ln -s "${srcdir}/printer/meshes/FlashforgeFinderBed.stl" "${version}/meshes/FlashforgeFinderBed.stl"
+	else
+		echo "FlashForgeFinderBed.stl already installed."
+	fi
 done
